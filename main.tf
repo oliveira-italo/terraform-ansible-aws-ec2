1b0e4fbd6cecc0a02696138b13c82582651f269a
oliveira-italo <arievilo.olati@gmail.com>
2024-03-14 21:04:29 -0300

    ansible

--- main.tf
+++ main.tf
@@ -11,14 +11,20 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0eb199b995e2bc4e3" // código do sistema operacional que rodará na instancia, disponível no AMI catalog na AWS
-  instance_type = "t2.micro" // tipo de instancia aws, com configurações próprias de hardware
-  key_name = "iac-alura" // chave ssh, criada no console aws (EC2 -> KEY PAIRS), será usada para acessar a instancia (ver connect ssh no console do EC2)
+  instance_type = "t2.micro"              // tipo de instancia aws, com configurações próprias de hardware
+  key_name      = "iac-alura"             // chave ssh, criada no console aws (EC2 -> KEY PAIRS), será usada para acessar a instancia (ver connect ssh no console do EC2)
+  # user_data     = <<-EOF
+  #                 #!/bin/bash
+  #                 cd /home/ubuntu
+  #                 echo "<h1>The book is on the table</h1>" > index.html
+  #                 nohup busybox httpd -f -p 8080 &
+  #               EOF
   tags = {
-    Name = "Primeira instância"
+    Name = "aws-ansible"
   }
 }
